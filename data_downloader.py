8f3a3dab9a578deaa60e8ceab6eb3677a22ec6e0
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2017-03-15 00:06:46 +0100

    First commit.

---
+++ data_downloader.py
@@ -0,0 +1,76 @@
+import requests
+import json
+import csv
+import collections
+
+def get_token( username, password ):
+    payload = {'username': username, 'password': password}
+    r = requests.post('http://raspeul.hopto.org:8080/smarthive/user/auth', json=payload)
+    return json.loads(r.text)['token']
+
+def get_available_sounds( token, hive_id ):
+    payload = {'from' : '2010-01-11T12:12:10', 'to' : '2020-11-12T23:13:10'}
+    headers = {'token': token}
+    r = requests.get('http://raspeul.hopto.org:8080/smarthive/measurement/sound/' + str(hive_id) + '/getAvailableSounds', headers=headers, params=payload)
+    json_obj = json.loads(r.text)
+    idxs = list(json_obj.values())
+    timestamps = list(json_obj.keys())
+    return json_obj
+
+def read_last_download_id():
+    f = open('last_id.txt', 'r')
+    idx = f.read()
+    f.close()
+    return idx;
+
+def write_last_download_id( idx ):
+    f = open('last_id.txt', 'r+')
+    f.truncate()
+    f.write(str(idx))
+    f.close()
+
+def create_csv_file( probes, file_name ):
+    writer = csv.writer(open('csvresults/' + file_name + '-soundvalues.csv', 'w', newline=''),
+                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    writer.writerow(['probe value'])
+    for probe in probes:
+        writer.writerow([int(probe)])
+
+def get_probes_create_charts( token, hive_id, sound_dict, start_idx ):
+    Sound = collections.namedtuple('Sound', 'id timestamp')
+    last_idx = start_idx;
+    sounds_sorted = sorted([Sound(v,k) for (k,v) in sound_dict.items()])
+    for sound in sounds_sorted:
+        if (start_idx < sound.id):
+            payload = {'soundId' : sound.id}
+            headers = {'token': token}
+            r = requests.get('http://raspeul.hopto.org:8080/smarthive/measurement/sound/' + str(hive_id), headers=headers, params=payload)
+            probesMic1 = json.loads(r.text)['valuesMic1']
+            create_csv_file(probesMic1, 'mic1/' + str(sound.id) + '-' + sound.timestamp)
+            probesMic2 = json.loads(r.text)['valuesMic2']
+            create_csv_file(probesMic2, 'mic2/' + str(sound.id) + '-' + sound.timestamp)
+            probesMic3 = json.loads(r.text)['valuesMic3']
+            create_csv_file(probesMic3, 'mic3/' + str(sound.id) + '-' + sound.timestamp)
+            probesMic4 = json.loads(r.text)['valuesMic4']
+            create_csv_file(probesMic4, 'mic4/' + str(sound.id) + '-' + sound.timestamp)
+            last_idx = sound.id;
+
+    return last_idx;
+
+def main():
+    #Get token for Authorization
+    token = get_token( 'tymons', '43210')
+
+    #Get ids of all avaliable sounds for hive with specific id
+    sounds_map = get_available_sounds( token, 2 )
+    #Read where you finished last time
+    start_idx = read_last_download_id();
+
+    last_idx = get_probes_create_charts(token, 2, sounds_map, int(start_idx) )
+    #Create
+    write_last_download_id(str(last_idx))
+
+
+if __name__ == "__main__":
+    main()
